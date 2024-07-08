dfd1f27c83786a10a6a194f26cdd001c55b3249c
rahulrajakka <akkarahulraj07@gmail.com>
2024-07-08 16:14:19 -0400

    remove zero rows

--- CNNModel.py
+++ CNNModel.py
@@ -1,9 +1,12 @@
 
 import pandas as pd
 
+# Load the sales data
+print("Loading sales data...")
 df = pd.read_csv('Download file and add file path ')
 print("Sales data loaded successfully.")
 print("DataFrame columns:", df.columns)
 
 
+# Remove rows where any column contains a zero value
 df = df[(df != 0).all(axis=1)]
