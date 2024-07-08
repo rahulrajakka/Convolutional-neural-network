74e3648a0615a37fe7fc12f168a3e7b22ee08820
rahulrajakka <akkarahulraj07@gmail.com>
2024-07-08 17:11:03 -0400

    Normalizing data code. Used .head to represent to 5 rowns.

--- CNNModel.py
+++ CNNModel.py
@@ -18,3 +18,11 @@ avg_sales['sales_product_quantity'] = avg_sales['sales_product_quantity'].astype
 avg_sales.columns = ['product_id', 'sales_week of year', 'avg_sales_product_quantity']
 print("Average sales calculated.")
 print(avg_sales.head())
+
+
+# Normalize the data
+print("Normalizing the data...")
+avg_sales['product_id'] = avg_sales['product_id'] / avg_sales['product_id'].max()
+avg_sales['sales_week of year'] = avg_sales['sales_week of year'] / avg_sales['sales_week of year'].max()
+print("Data normalized.")
+print(avg_sales.head())
