77c95fa8d474516d52afaf6646e963e7e94ab3b9
Deepnil <deepnil.vasava2899@gmail.com>
2024-07-09 22:25:51 -0400

    Bug#1 fix, null values are removed.

--- CNNModel.py
+++ CNNModel.py
@@ -1,16 +1,19 @@
-from tensorflow.keras.models import Sequential, load_model
+
 from tensorflow.keras.layers import Dense, Conv1D, Flatten
 import pandas as pd
 
 # Load the sales data
 print("Loading sales data...")
-df = pd.read_csv('Download file and add file path ')
+df = pd.read_csv(r'C:\Users\Owner\Desktop\Capstone\data_File\sales_data_V1.csv')
 print("Sales data loaded successfully.")
 print("DataFrame columns:", df.columns)
 
 
 # Remove rows where any column contains a zero value
 df = df[(df != 0).all(axis=1)]
+
+# Remove rows with any null values
+df.dropna(inplace=True)
 
 # Calculate average sales
 print("Calculating average sales...")
